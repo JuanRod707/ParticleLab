3f2e55f21d6e3345f487afcc7e8b1ccefb3b574a
Juan Rodriguez <juan.rodriguez@etermax.com.uy>
2018-02-26 12:29:55 -0300

    Added all particles

---
+++ Assets/Code/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float MinStartForce;
+    public float MaxStartForce;
+
+    private void Start()
+    {
+        var body = GetComponent<Rigidbody>();
+        body.AddForce(transform.forward * Random.Range(MinStartForce, MaxStartForce));
+    }
+}
